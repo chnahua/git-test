d457c66560203644de640d55c1cf342ff6208dc9
chnahua <chnahua@foxmail.com>
2021-10-14 15:12:50 +0800

    Idea First Commit

---
+++ src/main/java/com/ahua/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.ahua.git;
+
+/**
+ * @author huajun
+ * @create 2021-10-14 15:07
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
